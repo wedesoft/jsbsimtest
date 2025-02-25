3a9344930892a58bc07432ea0ce1c588ff5960eb
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-02-25 22:19:40 +0000

    Get aerodynamics forces in wind and body system

--- jsbsimtest.cpp
+++ jsbsimtest.cpp
@@ -1,5 +1,7 @@
+#include <memory>
 #include <iostream>
 #include <FGFDMExec.h>
+#include <initialization/FGInitialCondition.h>
 
 using namespace std;
 using namespace JSBSim;
@@ -8,4 +10,20 @@ int main(void)
 {
   FGFDMExec fdmex;
   fdmex.LoadModel("Shuttle");
+  shared_ptr<FGInitialCondition> fgic = fdmex.GetIC();
+  fgic->InitializeIC();
+  fgic->SetMachIC(0.8);
+  fgic->SetAlphaDegIC(10.0);
+  fgic->SetAltitudeAGLFtIC(1000.0);
+  fdmex.GetPropagate()->SetInitialState(fgic.get());
+  fdmex.RunIC();
+  cout << "forces in wind system: "
+       << fdmex.GetPropertyValue("forces/fwx-aero-lbs") << ", "
+       << fdmex.GetPropertyValue("forces/fwy-aero-lbs") << ", "
+       << fdmex.GetPropertyValue("forces/fwz-aero-lbs") << endl;
+  cout << "forces in body system: "
+       << fdmex.GetPropertyValue("forces/fbx-aero-lbs") << ", "
+       << fdmex.GetPropertyValue("forces/fby-aero-lbs") << ", "
+       << fdmex.GetPropertyValue("forces/fbz-aero-lbs") << endl;
+  return 0;
 }
