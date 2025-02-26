788bd2a47c45142ab48adc34b1b96b5ce6cb52ff
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-02-26 21:36:14 +0000

    Stable approach working (disabled elevator positioning delay

--- jsbsimtest.cpp
+++ jsbsimtest.cpp
@@ -12,30 +12,37 @@ int main(void)
   fdmex.LoadModel("Shuttle");
   shared_ptr<FGInitialCondition> fgic = fdmex.GetIC();
   fgic->InitializeIC();
-  fgic->SetMachIC(0.6);
-  fgic->SetAlphaDegIC(10.0);
-  fgic->SetAltitudeAGLFtIC(1000.0);
+  fgic->SetMachIC(0.3);
+  fgic->SetFlightPathAngleDegIC(-22.0);
+  fgic->SetAlphaDegIC(8.0);
+  fgic->SetAltitudeAGLFtIC(6000.0);
   fdmex.GetPropagate()->SetInitialState(fgic.get());
-  fdmex.SetPropertyValue("fcs/elevator-pos-norm", +1.0);
-  fdmex.SetPropertyValue("fcs/flap-pos-norm", 0.0);
-  fdmex.RunIC();
+  fdmex.SetPropertyValue("fcs/elevator-pos-rad", -0.31);
   for (auto prop: fdmex.GetPropertyCatalog())
     cout << prop << endl;
-  cout << "forces in wind system: "
-       << fdmex.GetPropertyValue("forces/fwx-aero-lbs") << ", "
-       << fdmex.GetPropertyValue("forces/fwy-aero-lbs") << ", "
-       << fdmex.GetPropertyValue("forces/fwz-aero-lbs") << endl;
-  cout << "forces in body system: "
-       << fdmex.GetPropertyValue("forces/fbx-aero-lbs") << ", "
-       << fdmex.GetPropertyValue("forces/fby-aero-lbs") << ", "
-       << fdmex.GetPropertyValue("forces/fbz-aero-lbs") << endl;
-  cout << "aerodynamic moment: "
-       << fdmex.GetPropertyValue("moments/l-aero-lbsft") << ", "
-       << fdmex.GetPropertyValue("moments/m-aero-lbsft") << ", "
-       << fdmex.GetPropertyValue("moments/n-aero-lbsft") << endl;
-  cout << "total moment: "
-       << fdmex.GetPropertyValue("moments/l-total-lbsft") << ", "
-       << fdmex.GetPropertyValue("moments/m-total-lbsft") << ", "
-       << fdmex.GetPropertyValue("moments/n-total-lbsft") << endl;
+  fdmex.RunIC();
+  for (int i=0; i<2000; i++) {
+    fdmex.Run();
+    cout << "velocities: "
+      << fdmex.GetPropertyValue("velocities/v-north-fps") << ", "
+      << fdmex.GetPropertyValue("velocities/v-east-fps") << ", "
+      << fdmex.GetPropertyValue("velocities/v-down-fps") << "; ";
+    // cout << "forces in wind system: "
+    //   << fdmex.GetPropertyValue("forces/fwx-aero-lbs") << ", "
+    //   << fdmex.GetPropertyValue("forces/fwy-aero-lbs") << ", "
+    //   << fdmex.GetPropertyValue("forces/fwz-aero-lbs") << "; ";
+    // cout << "total forces in body system: "
+    //      << fdmex.GetPropertyValue("forces/fbx-total-lbs") << ", "
+    //      << fdmex.GetPropertyValue("forces/fby-total-lbs") << ", "
+    //      << fdmex.GetPropertyValue("forces/fbz-total-lbs") << "; ";
+    cout << "; aerodynamic moment: "
+      << fdmex.GetPropertyValue("moments/l-aero-lbsft") << ", "
+      << fdmex.GetPropertyValue("moments/m-aero-lbsft") << ", "
+      << fdmex.GetPropertyValue("moments/n-aero-lbsft") << endl;
+    // cout << "total moment: "
+    //      << fdmex.GetPropertyValue("moments/l-total-lbsft") << ", "
+    //      << fdmex.GetPropertyValue("moments/m-total-lbsft") << ", "
+    //      << fdmex.GetPropertyValue("moments/n-total-lbsft") << endl;
+  };
   return 0;
 }
